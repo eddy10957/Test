56d6b47ab65c134668b32dc4a09ec2c268e8d6db
eddy10957 <eddytd96@gmail.com>
2021-10-19 17:14:36 +0200

    Initial Commit

---
+++ Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by Edoardo Troianiello on 19/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

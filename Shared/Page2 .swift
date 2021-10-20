34cce76c27c56f7cdebdf14e0dc683b86912eacd
AndreaMasturzo <andreamasturzo@outlook.com>
2021-10-20 14:06:25 +0200

    List

--- Shared/Page2 .swift
+++ Shared/Page2 .swift
@@ -7,9 +7,69 @@
 
 import SwiftUI
 
-struct Page2_: View {
+
+// Single cell structure
+struct Cell: View {
+    
+    @State var check = false
+    @State var sheetPresented = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            
+            // Check button on the left of the cell
+                Image(systemName: check ? "checkmark.circle.fill" : "circle")
+                    .font(.title)
+                    .onTapGesture {
+                        check.toggle()
+                    }
+                
+                .padding()
+            Spacer()
+            
+            VStack{
+                Text("ToDoTitle")
+                    .font(.title)
+                Text("Description")
+                    .font(.subheadline)
+                    .foregroundColor(Color.gray)
+            }
+            Spacer()
+            
+            // Info button on the right of the cell
+            Image(systemName: sheetPresented ? "info.circle.fill" : "info.circle")
+                    .font(.title)
+                    .foregroundColor(.blue)
+                    .onTapGesture {
+                        sheetPresented.toggle()
+                    }
+                .padding()
+        }
+        if sheetPresented == true {
+            sheetView()
+        }
+    }
+    
+}
+
+
+// SheetView
+struct sheetView: View {
+    
+    var body: some View {
+        Text("This will be a full view")
+    }
+}
+
+struct Page2_: View {
+    
+    // List
+    var body: some View {
+        NavigationView {
+            List(0 ..< 6) { item in
+                Cell()
+            }
+        }
     }
 }
 

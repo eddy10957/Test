98f0327754e14af466fde837dbc9c4127dc8141f
eddy10957 <eddytd96@gmail.com>
2021-10-20 10:52:23 +0200

    Added New Page and Navigation link for it in Content view

---
+++ Shared/Page3.swift
@@ -0,0 +1,20 @@
+//
+//  Page3.swift
+//  Test
+//
+//  Created by Edoardo Troianiello on 20/10/21.
+//
+
+import SwiftUI
+
+struct Page3_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Page3__Previews: PreviewProvider {
+    static var previews: some View {
+        Page3_()
+    }
+}
